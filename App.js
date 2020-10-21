cf0e9a0e39c9131a9d1cb1b69dac06fa7cf72451
matheuscarv69 <matheus9126@gmail.com>
2020-10-21 17:57:59 -0400

    Create PeopleListItem

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {  View, Text  } from "react-native";
+import Header from "./src/components/Header";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import PeopleList from "./src/components/PeopleList";
+
+import axios from "axios";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      peoples: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://randomuser.me/api/?nat=br&results=5")
+      .then((response) => {
+        const { results } = response.data;
+       
+        this.setState({
+          peoples: results
+        })
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas" />
+          <PeopleList peoples={this.state.peoples} />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/PeopleListItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const PeopleListItem = (props) => {
+  const { people } = props;
+  const { title, first, last } = people.name;
+
+  return (
+    <View style={styles.line}>
+      <Text style={styles.lineText}>
+        { `${title} ${first} ${last}` }
+      </Text>
+    </View>
+  );
+
+ 
+};
+
+const styles = StyleSheet.create({
+  line: {
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: "#bbb",
+
+    justifyContent: "center",
+    // alignItems: "center",
+    // flexDirection: "row"
+  },
+  lineText: {
+    fontSize: 20,
+    paddingLeft: 15,
+  },
+});
+
+export default PeopleListItem;
