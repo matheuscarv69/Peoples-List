621166dd78c4a3f1275d6be188763973b01eebc3
matheuscarv69 <matheus9126@gmail.com>
2020-10-26 10:23:05 -0400

    TouchableOpacity implemented in PeopleListItem

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-
-// teste
 const PeopleListItem = (props) => {
   const { people } = props;
   const { title, first, last } = people.name;
 
   return (
-    <View style={styles.line}>
-      <Text style={styles.lineText}>
-        { `${title} ${first} ${last}` }
-      </Text>
-    </View>
+    <TouchableOpacity onPress={() => ) }>
+      <View style={styles.line}>
+        <Image style={styles.avatar} source={{ uri: people.picture.thumbnail }}/>
+        <Text style={styles.lineText}>{`${title} ${first} ${last}`}</Text>
+      </View>
+    </TouchableOpacity>
   );
-
- 
 };
 
 const styles = StyleSheet.create({
@@ -24,13 +21,23 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#bbb",
 
-    justifyContent: "center",
-    // alignItems: "center",
-    // flexDirection: "row"
+    // justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
   },
   lineText: {
     fontSize: 20,
     paddingLeft: 15,
+
+    flex: 7,
+  },
+  avatar: {
+    aspectRatio: 1,
+
+    marginLeft: 15,
+    borderRadius: 50,
+
+    flex: 1,
   },
 });
 
