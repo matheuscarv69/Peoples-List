e14244111db0b16ea63ecc6c0f9a156eea0b4011
matheuscarv69 <matheus9126@gmail.com>
2020-11-04 13:03:55 -0400

    Implementation of the Activity Indicator (loading component) and "error handling"

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
 
 import PeopleListItem from "./PeopleListItem";
 
 const PeopleList = (props) => {
   const { peoples, onPressItem } = props;
 
-  const items = peoples.map((people) => (
-    <PeopleListItem 
-      key={people.name.first} 
-      people={people} 
-      navigateToPeopleDetail={onPressItem}/>
-  ));
-
-  return <View style={styles.container}>{items}</View>;
+  return ( <FlatList 
+          style={styles.container}
+          data={peoples}
+          renderItem={( { item }) => (
+              <PeopleListItem 
+                    people={item} 
+                    navigateToPeopleDetail={onPressItem}/>
+          )} 
+          keyExtractor={ item => item.login.uuid }
+          />
+  );
 };
 
 const styles = StyleSheet.create({
--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,6 +1,6 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {  View, Text  } from "react-native";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 
 import PeopleList from "../components/PeopleList";
 
@@ -11,32 +11,81 @@ export default class PeoplePage extends React.Component {
     super(props);
 
     this.state = {
-      peoples: []
+      peoples: [],
+      loading: false,
+      error: false,
     };
   }
-  
+
   componentDidMount() {
+    this.setState({ loading: true });
+
     axios
-      .get("https://randomuser.me/api/?nat=br&results=5")
+      .get("https://randomuser.me/api/?nat=br&results=100")
       .then((response) => {
         const { results } = response.data;
-       
+
         this.setState({
-          peoples: results
-        })
+          peoples: results,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+          error: true,
+        });
       });
+  }
+
+  renderPage() {
+    if (this.state.loading) {
+      return <ActivityIndicator size="large" color="#6ca2f7" />;
+    }
+    if (this.state.error) {
+      return <Text style={styles.error}>Deu pau aqui!</Text>;
+    }
+    return (
+      <PeopleList
+        peoples={this.state.peoples}
+        onPressItem={(pageParams) => {
+          this.props.navigation.navigate("PeopleDetail", pageParams);
+        }}
+      />
+    );
   }
 
   render() {
     return (
-      <View>
-          <PeopleList 
-          peoples={this.state.peoples} 
-          onPressItem={pageParams => {
-            // this.props.navigation.navigate(/* chave da página */, /* state */)
-            this.props.navigation.navigate('PeopleDetail', pageParams)
-          }} />
+      <View style={styles.container}>
+        {this.renderPage()}
+        
+        {/* Mesma função de loading usando operador ternário 
+        
+        {this.state.loading ? (
+          <ActivityIndicator size="large" color="#6ca2f7" />
+        ) : this.state.error ? (
+          <Text style={styles.error}> DEU PAU!!!</Text>
+        ) : (
+          <PeopleList
+            peoples={this.state.peoples}
+            onPressItem={(pageParams) => {
+              this.props.navigation.navigate("PeopleDetail", pageParams);
+            }}
+          />
+        )} */}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  error: {
+    color: "red",
+    alignSelf: "center",
+  },
+});
